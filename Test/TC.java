a586e61ac529ca0bdaca592a77c22de1ec5194a6
tc <tc@RangyDev>
2019-06-14 19:29:25 -0500

    first commit

---
+++ Test/TC.java
@@ -0,0 +1,23 @@
+package test;
+
+
+public class TC {
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Mmmeoowww");
+		System.out.println("Test");
+		int a = 1;
+		int b = 2; 
+		int c;
+		c = a + b;
+		System.out.println(c);
+	}
+		
+}
+
+
